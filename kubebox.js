a4b1f9c679e56324e87546f554a45522855ab12c
Горшков Илья <gorshkov@skbkontur.ru>
2019-03-28 12:00:52 +0500

    fixed namespace

--- kubebox.js
+++ kubebox.js
@@ -3880,95 +3880,94 @@ function namespaces_list(screen) {
 
 function prompt(screen, client, { current_namespace, promptAfterRequest } = { promptAfterRequest : false }) {
   return new Promise(function(fulfill, reject) {
-    const list = namespaces_list(screen);
-    const { until } = spinner(screen);
-    let namespaces = [], message;
+    // const list = namespaces_list(screen);
+    // const { until } = spinner(screen);
+    // let data = ['k8s-hello-world'];
 
-    // TODO: watch for namespaces changes when the selection list is open
-    function request_namespaces() {
-      return (client.openshift ? client.projects().get() : client.namespaces().get())
-        .then(response => JSON.parse(response.body.toString('utf8')))
-        .then(response => namespaces = response)
-        .then(namespaces => namespaces.items.length === 0
-          ? list_message('No available namespaces')
-          : list.setItems(namespaces.items.reduce((data, namespace) => {
-            data.push(namespace.metadata.name === current_namespace
-              ? `{blue-fg}${namespace.metadata.name}{/blue-fg}`
-              : namespace.metadata.name);
-            return data;
-            }, [])))
-        .then(() => screen.render());
+    function getNamespace() {
+      const reg = /(?<=http(s)?:\/\/).*(?=.kube.devkontur.ru)/gi
+      // const reg = /(?<=http(s)?:\/\/).*(?=kube.testkontur.ru)/gi
+      return window.location.href.match(reg);
     }
+    const namespace = getNamespace();
 
-    function prompt_namespaces_list() {
-      screen.saveFocus();
-      screen.grabKeys = true;
-      screen.grabMouse = true;
-      screen.append(list);
-      list.focus();
-      list.grabMouse = true;
-      screen.render();
-    }
+    // function request_namespaces() {
+    //   return (client.namespaces().get())
+    //     .then(() => list.setItems(data, []))
+    //     .then(() => screen.render());
+    // }
 
-    function close_namespaces_list() {
-      list.detach();
-      screen.restoreFocus();
-      screen.grabKeys = false;
-      screen.grabMouse = false;
-      screen.render();
-    }
+    // function prompt_namespaces_list() {
+    //   screen.saveFocus();
+    //   screen.grabKeys = true;
+    //   screen.grabMouse = true;
+    //   screen.append(list);
+    //   list.focus();
+    //   list.grabMouse = true;
+    //   screen.render();
+    // }
 
-    function list_message(text, options = {}) {
-      if (message) message.destroy();
-      message = blessed.text(Object.assign({
-        parent  : list,
-        tags    : true,
-        top     : '50%-1',
-        left    : 'center',
-        width   : 'shrink',
-        height  : 'shrink',
-        align   : 'center',
-        valign  : 'middle',
-        content : text,
-      }, options));
-    }
+    // function close_namespaces_list() {
+    //   list.detach();
+    //   screen.restoreFocus();
+    //   screen.grabKeys = false;
+    //   screen.grabMouse = false;
+    //   screen.render();
+    // }
+
+    // function list_message(text, options = {}) {
+    //   if (message) message.destroy();
+    //   message = blessed.text(Object.assign({
+    //     parent  : list,
+    //     tags    : true,
+    //     top     : '50%-1',
+    //     left    : 'center',
+    //     width   : 'shrink',
+    //     height  : 'shrink',
+    //     align   : 'center',
+    //     valign  : 'middle',
+    //     content : text,
+    //   }, options));
+    // }
 
     if (promptAfterRequest) {
-      request_namespaces()
-        .then(prompt_namespaces_list)
-        .catch(error => reject(error));
-    } else {
-      prompt_namespaces_list();
-      until(request_namespaces())
-        .spin(s => list.setLabel(`${s} Namespaces`))
-        .then(_ => list.setLabel('Namespaces'))
-        .catch(error => {
-          close_namespaces_list();
-          reject(error);
-        });
-    }
+      fulfill(namespace);
+      // request_namespaces()
+      //   .then(prompt_namespaces_list)
+      //   .catch(error => reject(error));
+    } 
+    // else {
+    //   prompt_namespaces_list();
+    //   until(request_namespaces())
+    //     .spin(s => list.setLabel(`${s} Namespaces`))
+    //     .then(_ => list.setLabel('Namespaces'))
+    //     .catch(error => {
+    //       close_namespaces_list();
+    //       reject(error);
+    //     });
+    // }
 
-    list.on('cancel', () => {
-      close_namespaces_list();
-      fulfill(current_namespace);
-    });
+    // list.on('cancel', () => {
+    //   close_namespaces_list();
+    //   fulfill(current_namespace);
+    // });
 
-    list.on('key q', () => {
-      if (os.platform() !== 'browser') {
-        process.exit(0);
-      }
-    });
+    // list.on('key q', () => {
+    //   if (os.platform() !== 'browser') {
+    //     process.exit(0);
+    //   }
+    // });
 
-    list.on('select', (item, i) => {
-      close_namespaces_list();
-      if (item) {
-        const namespace = namespaces.items[i].metadata.name;
-        fulfill(namespace);
-      } else {
-        // no namespaces
-        fulfill(current_namespace);
-      }
-    });
+    // list.on('select', (item, i) => {
+    //   close_namespaces_list();
+    //   if (item) {
+    //     const namespace = data[i];
+    //     fulfill(namespace);
+    //   } else {
+    //     // no namespaces
+    //     fulfill(current_namespace);
+    //   }
+    // });
   });
 }
 
